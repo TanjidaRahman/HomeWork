4eff11abf2607242c08925e0f2d45d94670baa2b
TanjidaRahman <tnjdrhmn@gmail.com>
2022-01-30 00:17:28 -0500

    01302022 12:17

--- src/hw6Q2IfElseCondition/Voter.java
+++ src/hw6Q2IfElseCondition/Voter.java
@@ -4,15 +4,15 @@ public class Voter {
 
 	public static void main(String[] args) {
 		int age = 25;
+
 		if (age == 18) {
 			System.out.println("I'm a voter");
 		} else if (age < 18) {
 			System.out.println("I'm not a voter");
-		} else if (age >= 18) {
+		} else if (age > 18) {
 			System.out.println("I'm a voter");
 		} else {
 			System.out.println("Please add a valid age");
 		}
 	}
-
 }
